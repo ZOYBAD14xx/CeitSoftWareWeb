77f958c4ff1812c0016a3219253a8a95debad60c
ZOYBAD14xx <60214442+ZOYBAD14xx@users.noreply.github.com>
2021-10-06 16:09:23 +0700

    Add files via upload

---
+++ WorkPast.js
@@ -0,0 +1,58 @@
+[
+  {
+    "id":1,
+    "title": "Workshop HTML ( Introduction to website )",
+    "Date": "10-11/2/2019",
+    "Description":"",
+    "img":""
+  },
+  {
+    "id":2,
+    "title": "Workshop HTML & CSS",
+    "Date": "16-17/11/2019",
+    "Description":"",
+    "img":""
+  },
+  {
+    "id":3,
+    "title": "Basic Programming Workshop",
+    "Date": "14-15/12/2019",
+    "Description":"",
+    "img":""
+  },
+  {
+    "id":4,
+    "title": "Hacking Workshop by CQ",
+    "Date": "11/7/2020",
+    "Description":"",
+    "img":""
+  },
+  {
+    "id":5,
+    "title": "Web Blog Workshop",
+    "Date": "18-19/7/2020",
+    "Description":"",
+    "img":""
+  },
+  {
+    "id":6,
+    "title": "ງານສຳພາດຮັບສະມາຊິກໃໝ່ Gen3",
+    "Date": "16/8/2020",
+    "Description":"",
+    "img":""
+  },
+  {
+    "id":7,
+    "title": "Workshop HTML & CSS",
+    "Date": "16-17/1/2021",
+    "Description":"",
+    "img":""
+  },
+  {
+    "id":8,
+    "title": "Python for webdev Workshop",
+    "Date": "28/3/2021",
+    "Description":"",
+    "img":""
+  },
+]
